a6bcabd435c380c4741fdaeb2f1c2fb72307e325
TUNKSTUN <johnwick4learning@gmail.com>
2023-03-28 00:48:24 +0530

    ...

--- src/Components/GitHubStarButton.js
+++ src/Components/GitHubStarButton.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Component.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Component.css";
 
 function GitHubStarButton({ owner, repo }) {
   const [isStarred, setIsStarred] = useState(false);
@@ -8,21 +8,33 @@ function GitHubStarButton({ owner, repo }) {
 
   useEffect(() => {
     async function checkIfStarred() {
-      try {
-        const response = await axios.get(`https://api.github.com/user/starred/${owner}/${repo}`, {
-          headers: {
-            Authorization: `Bearer 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`,
-          },
-        });
-        setIsStarred(true);
-      } catch (error) {
-        setIsStarred(false);
+        try {
+          await axios.get(
+            `https://api.github.com/user/starred/${owner}/${repo}`,
+            {
+              headers: {
+                Authorization: `Bearer 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`,
+              },
+            }
+          );
+          setIsStarred(true);
+        } catch (error) {
+          setIsStarred(false);
+        }
       }
-    }
+      
 
     async function getStarCount() {
       try {
-        const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}`);
+        const response = await axios.get(
+          `https://api.github.com/repos/${owner}/${repo}`,
+          {
+            headers: {
+              Authorization: `Bearer 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`,
+            },
+          }
+        );
+
         setStarCount(response.data.stargazers_count);
       } catch (error) {
         console.error(error);
@@ -36,23 +48,30 @@ function GitHubStarButton({ owner, repo }) {
   const handleStarClick = async () => {
     try {
       if (isStarred) {
-        await axios.delete(`https://api.github.com/user/starred/${owner}/${repo}`, {
-          headers: {
-            Authorization: `Bearer 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`,
-          },
-        });
+        await axios.delete(
+          `https://api.github.com/user/starred/${owner}/${repo}`,
+          {
+            headers: {
+              Authorization: `Bearer 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`,
+            },
+          }
+        );
         setIsStarred(false);
-        setStarCount(prevCount => prevCount - 1);
-        console.log('Unstarred');
+        setStarCount((prevCount) => prevCount - 1);
+        console.log("Unstarred");
       } else {
-        await axios.put(`https://api.github.com/user/starred/${owner}/${repo}`, null, {
-          headers: {
-            Authorization: `Bearer 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`,
-          },
-        });
+        await axios.put(
+          `https://api.github.com/user/starred/${owner}/${repo}`,
+          null,
+          {
+            headers: {
+              Authorization: `Bearer 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`,
+            },
+          }
+        );
         setIsStarred(true);
-        setStarCount(prevCount => prevCount + 1);
-        console.log('Starred');
+        setStarCount((prevCount) => prevCount + 1);
+        console.log("Starred");
       }
     } catch (error) {
       console.error(error);
@@ -60,11 +79,14 @@ function GitHubStarButton({ owner, repo }) {
   };
 
   return (
-    <div className='text-center justify-center items-center flex flex-col'>
-      <button onClick={handleStarClick} className='github-star-button flex justify-center items-center text-center p-1 h-6 m-0'>
-        {isStarred ? '⭐' : '⭐'}
+    <div className="text-center justify-center items-center flex flex-col">
+      <button
+        onClick={handleStarClick}
+        className="github-star-button flex justify-center items-center text-center p-1 h-6 m-0 drop-shadow-lg"
+      >
+        {isStarred ? "⭐" : "⭐"}
       </button>
-      <p className='text-xs font-bold drop-shadow-sm'>{starCount} stars</p>
+      <p className="text-xs font-bold drop-shadow-lg">{starCount} {starCount > 1 && starCount < 0 ? "stars":"star"}</p>
     </div>
   );
 }
